2d3bf53786540fd8ac75e5ff80fa87351206e46e
wadrian araujo <wadm.snf20@uea.edu.br>
2022-02-04 23:47:50 -0400

    Add client

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
 
-const db = require('./models')
+app.use(express.json());
+app.use(cors());
 
+const db = require('./models');
+
+//Routers
+const postRouter = require('./routes/Posts');
+app.use("/posts",postRouter);
 
 
 db.sequelize.sync().then(()=> {
     app.listen(port, () => { 
-        console.log(`Example app listening on port ${port}!`)
-        console.log("Pega a Fita")
+        console.log(`Example app listening on port ${port}!`);
     });
-})
+});
 
