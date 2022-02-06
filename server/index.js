97497647df12c4c52674222b0080a8a0d8551e62
wadrian araujo <wadm.snf20@uea.edu.br>
2022-02-06 00:14:42 -0400

    add class 8

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const port = 3001;
+const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
-const db = require('./models');
+const db = require("./models");
 
-//Routers
-const postRouter = require('./routes/Posts');
-app.use("/posts",postRouter);
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
 
-
-db.sequelize.sync().then(()=> {
-    app.listen(port, () => { 
-        console.log(`Example app listening on port ${port}!`);
-    });
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
 
